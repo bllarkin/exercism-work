836a3957242bba4469a739d8b3aaf9a7cf3e12d1
Brandon Larkin <bllarkin@gmail.com>
2022-08-27 14:35:14 -0700

    Complete Logs Logs Logs exercise

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,5 +1,7 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 	recRune := '\u2757'
@@ -24,11 +26,22 @@ func Application(log string) string {
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := []rune(log)
+
+	for index, char := range newLog {
+		if char == oldRune {
+			newLog[index] = newRune
+		}
+	}
+	return string(newLog)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	if utf8.RuneCountInString(log) <= limit {
+		return true
+	} else {
+		return false
+	}
 }
